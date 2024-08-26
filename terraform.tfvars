5db76341a3dc54d2399cfb6747fc10e1c2f0cda0
ARYANNJ <aryanjadhav71.gmail.com>
2024-08-26 15:48:44 +0530

    added subnet

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+vpc_cidr            = "18.0.0.0/16"
+Public_subnet_cidr  = "18.0.0.0/17"
+Private_subnet_cidr = "18.0.128.0/17"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  description = "this is cidr for vpc"
+}
+variable "Public_subnet_cidr" {
+  description = "Public subnet"
+}
+variable "Private_subnet_cidr" {
+  description = "Private subnet"
+}
